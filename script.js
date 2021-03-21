b9d6279e83ce79575e821905eb2a43b1133f31d2
gaelporteu <gael.porteu@gmail.com>
2021-03-20 22:36:21 -0400

    Added array filter tests

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const companies = [
   { name: "Company One", category: "Finance", start: 1981, end: 2003 },
   { name: "Company Two", category: "Retail", start: 1989, end: 2020 },
   { name: "Company Three", category: "Auto", start: 1979, end: 2008 },
-  { name: "Company Four", category: "Technology", start: 1990, end: 2001 },
+  { name: "Company Four", category: "Technology", start: 1990, end: 1998 },
   { name: "Company Five", category: "Home", start: 1986, end: 2016 },
 ];
 
@@ -29,12 +29,24 @@ const ages = [33, 12, 20, 16, 5, 54, 44, 61, 13, 15, 46, 103];
 //     return true;
 //   }
 // });
-const canDrink = ages.filter((age) => age >= 21);
+// const canDrink = ages.filter((age) => age >= 21);
 
-console.log(canDrink);
+// console.log(canDrink);
+
+const retailCompanies = companies.filter(company => company.category === "Retail");
+
+console.log(retailCompanies);
+
+// Get 1980s companies
+const eightiesCompanies = companies.filter(company => company.start > 1979 && company.start < 1990 );
+
+console.log(eightiesCompanies);
+// Get companies that lasted 10 yrs+
+const haulerCompanies = companies.filter(company => (company.end - company.start) >= 10);
+
+console.log(haulerCompanies);
 
 // map
-
 
 // sort
 // reduce
